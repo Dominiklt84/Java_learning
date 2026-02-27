a9a8c7dd48b75985c913cc81d930dcc8808d406b
Dominiklt84 <dominik.alencinovic@stud.viko.lt>
2026-02-27 14:54:17 +0200

    27.02.2026

--- first-spring-boot/src/main/java/lt/viko/eif/dalencinovic/first/spring/service/CommandLineRunnerImpl.java
+++ first-spring-boot/src/main/java/lt/viko/eif/dalencinovic/first/spring/service/CommandLineRunnerImpl.java
@@ -1,24 +1,17 @@
 package lt.viko.eif.dalencinovic.first.spring.service;
 
-import lt.viko.eif.dalencinovic.first.spring.db.CustomerRepository;
-import lt.viko.eif.dalencinovic.first.spring.model.Customer;
+import lt.viko.eif.dalencinovic.first.spring.menu.UserMenu;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-
 @Component
 public class CommandLineRunnerImpl implements CommandLineRunner {
     @Autowired
-    private CustomerRepository customerRepository;
+    private UserMenu userMenu;
 
     @Override
-    public void run(String... args)throws Exception{
-        List<Customer> customers=customerRepository.findAll();
-        for(Customer customer:customers){
-            System.out.println(customer);
-        }
-        System.in.read();
+    public void run(String... args) throws Exception{
+        userMenu.showMenu();
     }
 }
