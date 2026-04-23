770b24b320310dc8091a5a36d8299a02efb15433
Dominiklt84 <dominik.alencinovic@stud.viko.lt>
2026-04-23 20:33:54 +0300

    23.04.2026

---
+++ webservice/src/test/java/lt/viko/eif/dalencinovic/webservice/WebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.viko.eif.dalencinovic.webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
